0c577543377e51d228628303e875bf211ececf47
miaoxinguo <miaoxinguo@jd.com>
2016-09-05 20:15:23 +0800

    add mybtais generator

--- src/main/java/demo/spring/boot/controller/UserController.java
+++ src/main/java/demo/spring/boot/controller/UserController.java
@@ -1,19 +1,22 @@
 package demo.spring.boot.controller;
 
 import demo.spring.boot.entity.User;
-import org.springframework.validation.BindingResult;
+import demo.spring.boot.repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping(value = "user", method = RequestMethod.GET)
-    public String getUser(@Valid User user, BindingResult result) {
-        System.out.println(result);
-        return user.getName();
+    public String getUser(long userId) {
+        System.out.println(userId);
+        User user = userMapper.selectByPrimaryKey(userId);
+        return user.toString();
     }
 }
