17372a1d12ca4253b4e12512888058f3db6f4ce5
miaoxinguo <miaoxinguo2002@163.com>
2017-01-18 00:24:36 +0800

    use jackson replace fastjson

--- sbs-core/src/main/java/io/github/miaoxinguo/sbs/util/JsonUtils.java
+++ sbs-core/src/main/java/io/github/miaoxinguo/sbs/util/JsonUtils.java
@@ -1,211 +1,79 @@
 package io.github.miaoxinguo.sbs.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
-import org.apache.commons.lang3.StringUtils;
-
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.SerializationException;
 
 import java.util.List;
 
 /**
- * 按约定封装后台返回前台的数据
+ * Json 工具类 - Jackson 实现版本
+ *
+ * Jackson 所有异常转为 apache common 的 SerializationException 抛出， 由 spirng mvc 框架统一处理
  */
 public final class JsonUtils {
 
-    /*
-     *Fastjson的SerializerFeature序列化属性:
-     *
-     *  QuoteFieldNames———-输出key时是否使用双引号,默认为true
-     *  WriteMapNullValue——–是否输出值为null的字段,默认为false
-     *  WriteNullNumberAsZero—-数值字段如果为null,输出为0,而非null
-     *  WriteNullListAsEmpty—–List字段如果为null,输出为[],而非null
-     *  WriteNullStringAsEmpty—字符类型字段如果为null,输出为”“,而非null
-     *  WriteNullBooleanAsFalse–Boolean字段如果为null,输出为false,而非null
-     */
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        /*
+         * 该特性决定parser将是否允许解析使用Java/C++ 样式的注释（包括'/'+'*' 和'//' 变量）。
+         * 由于JSON标准说明书上面没有提到注释是否是合法的组成，所以这是一个非标准的特性；
+         * 尽管如此，这个特性还是被广泛地使用。
+         *
+         * 该属性默认是false
+         */
+        objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+
+        /*
+         * 这个特性决定parser是否将允许使用非双引号属性名字， （这种形式在Javascript中被允许，但是JSON标准说明书中没有）。
+         * 由于JSON标准上需要为属性名称使用双引号，所以这也是一个非标准特性，默认是false的。
+         */
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+
+        /*
+         * 该特性决定parser是否允许单引号来包住属性名称和字符串值。默认是false
+         */
+        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+
+        /*
+         * 该特性决定parser是否允许JSON字符串包含非引号控制字符（值小于32的ASCII字符，包含制表符和换行符）。
+         * 如果该属性关闭，则如果遇到这些字符，则会抛出异常。
+         * JSON标准说明书要求所有控制符必须使用引号，因此这是一个非标准的特性。 默认是false。
+         */
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+    }
+
     private JsonUtils() {
     }
 
     /**
-     * 通用方法
+     * 对象转json字符串
      */
     public static String toJsonString(Object object) {
-        return JSON.toJSONString(object,SerializerFeature.DisableCircularReferenceDetect);
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException();
+        }
     }
 
     /**
-     * 配合属性filter的转json方法
-     */
-    public static String toJsonString(Object object, SimplePropertyPreFilter filter) {
-        return JSON.toJSONString(object, filter);
-    }
-
-    /**
-     * 通用方法
-     */
-    public static JSONObject toJsonObject(String text) {
-        return JSON.parseObject(text);
-    }
-
-    /**
-     * 将JsonObject转换为对应的对象
+     * Json字符串转对象
      */
     public static <T> T parseObject(String text, Class<T> clazz) {
-        return JSON.parseObject(text, clazz);
+        return objectMapper.convertValue(text, clazz);
     }
 
     /**
-     * 将JsonArray转换为对应的List
+     * 将 JsonArray 转换为对应的List
      */
     public static <T> List<T> parseArray(String text, Class<T> clazz) {
-        return JSON.parseArray(text, clazz);
+        JavaType type = objectMapper.getTypeFactory().constructParametricType(List.class, clazz);
+        return objectMapper.convertValue(text, type);
     }
 
-    /**
-     * 返回成功标识和包含null作为空串的信息
-     */
-    public static String toSuccessJsonObjectIncludeNull(Object obj) {
-        return "{\"success\":true,\"msg\":" + toJsonStringIncludeNull(obj) + "}";
-    }
-
-    /**
-     * null值作为空串
-     */
-    public static String toJsonStringIncludeNull(Object object) {
-        SerializerFeature[] sf = {
-                SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullStringAsEmpty,
-                SerializerFeature.WriteNullListAsEmpty
-        };
-        return JSON.toJSONString(object, sf);
-    }
-
-    /**
-     * 只包含指定属性
-     *
-     * @param properties 属性名数组
-     */
-    public static String toJsonStringWithIncludes(Object object, String... properties) {
-        SimplePropertyPreFilter filter = new SimplePropertyPreFilter();
-        for (String p : properties) {
-            filter.getIncludes().add(p);
-        }
-        return toJsonString(object, filter);
-    }
-
-    /**
-     * 只包含指定属性
-     *
-     * @param properties 属性名集合
-     */
-    public static String toJsonStringWithIncludes(Object object, List<String> properties) {
-        return toJsonStringWithIncludes(object, properties.toArray(new String[0]));
-    }
-
-    /**
-     * 忽略object中的指定属性符合规范的json串
-     *
-     * @param properties 属性名
-     */
-    public static String toSuccessJsonStringWithExcludes(Object object, String... properties) {
-        SimplePropertyPreFilter filter = new SimplePropertyPreFilter();
-        for (String p : properties) {
-            filter.getExcludes().add(p);
-        }
-        return "{\"success\":true,\"msg\":" + toJsonString(object, filter) + "}";
-    }
-    /**
-     * 只包含指定属性符合规范的json串
-     *
-     * @param properties 属性名数组
-     */
-    public static String toSuccessJsonObjectWithIncludes(Object object, String... properties) {
-        SimplePropertyPreFilter filter = new SimplePropertyPreFilter();
-        for (String p : properties) {
-            filter.getIncludes().add(p);
-        }
-        return "{\"success\":true,\"msg\":" + toJsonString(object, filter) + "}";
-    }
-
-
-    /**
-     * 忽略object中的指定属性
-     *
-     * @param properties 属性名
-     */
-    public static String toJsonStringWithExcludes(Object object, String... properties) {
-        SimplePropertyPreFilter filter = new SimplePropertyPreFilter();
-        for (String p : properties) {
-            filter.getExcludes().add(p);
-        }
-        return toJsonString(object, filter);
-    }
-
-    /**
-     * 忽略object中的指定属性
-     *
-     * @param properties 属性名
-     */
-    public static String toJsonStringWithExcludes(Object object, List<String> properties) {
-        return toJsonStringWithExcludes(object, properties.toArray(new String[0]));
-    }
-
-    /**
-     * 返回成功标识
-     */
-    public static String toSuccessJson() {
-        return "{\"success\":true}";
-    }
-
-    /**
-     * 返回成功标识和信息
-     */
-    public static String toSuccessJson(final String msg) {
-        if (StringUtils.isBlank(msg)) {
-            return toSuccessJson();
-        }
-        return "{\"success\":true,\"msg\":\"" + msg + "\"}";
-    }
-
-    /**
-     * 返回成功标识和信息
-     */
-    public static String toSuccessJsonObject(Object obj) {
-        return "{\"success\":true,\"msg\":" + toJsonString(obj) + "}";
-    }
-
-    /**
-     * 返回成功标识和信息
-     *
-     * @param msg 必须是Json字符串 {}或[]结构
-     */
-    public static String toSuccessJsonObject(final String msg) {
-        if (StringUtils.isBlank(msg)) {
-            return toSuccessJson();
-        }
-        return "{\"success\":true,\"msg\":" + msg + "}";
-    }
-
-    /**
-     * 返回失败标识和信息
-     */
-    public static String toFailureJson() {
-        return toFailureJson("");
-    }
-
-    /**
-     * 返回失败标识和信息
-     */
-    public static String toFailureJson(final String msg) {
-        return toFailureJson(0, msg);
-    }
-
-    /**
-     * 返回失败标识、响应码、失败信息
-     */
-    public static String toFailureJson(final int code, final String msg) {
-        return "{\"success\":false,\"code\":" + code + ",\"msg\":\"" + msg + "\",\"body\":\"" + msg + "\"}";
-    }
 
 }
